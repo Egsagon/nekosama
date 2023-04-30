a2fdf529727d8944a80fe2e14a3c5a6e6fa92a3b
Egsagon <83862309+Egsagon@users.noreply.github.com>
2023-04-30 12:47:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from setuptools import setup
 
+classifiers = [ # idk what is this lets just assume its true
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
 if __name__ == "__main__":
-    setup()
+    setup(
+    name='nekosama',
+    version='3.0.1',
+    description='An API for the neko-sama website.',
+    long_description=open('README.md').read(),
+    url='https://github.com/Egsagon/neko-sama-api',
+    author='Egsagon',
+    author_email='egsagon12@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='nekosama',
+    packages=find_packages(),
+    install_requires=['']
+)
